188c4ff207b40f392f6b17fc5ed2c8ae62eeae9a
Irsyad Rabbani <irsyadrbbn19@gmail.com>
2022-06-12 01:39:58 +0700

    Create clearAll/AC function

--- script.js
+++ script.js
@@ -67,3 +67,16 @@ const calculate = () => {
     currentNumber = result
     calculationOperator = ''
 }
+
+const clearAll = () => {
+    prevNumber = ''
+    calculationOperator = ''
+    currentNumber = '0'
+}
+
+const clearBtn = document.querySelector('.all-clear')
+
+clearBtn.addEventListener('click', () => {
+    clearAll()
+    updateScreen(currentNumber)
+})
